331e339fec38f4678da4e86c0028459001cc73c1
Ionut Colceriu <contact@ghinda.net>
2016-01-26 11:59:58 +0200

    More work on the Bin theme and extra tests for it.

--- test/themes/bin.spec.js
+++ test/themes/bin.spec.js
@@ -42,4 +42,41 @@ describe('Bin Theme', function () {
     var paneWidth = parseInt(window.getComputedStyle($paneHtml).getPropertyValue('width'), 10)
     expect(paneWidth).toBe((containerWidth - 2) / 2)
   })
+
+  it('should take up 33.33% of the container with two file types', function () {
+    jotted.core = new Jotted(dom.$editor, {
+      files: [{
+        type: 'html',
+        content: ''
+      }, {
+        type: 'css',
+        content: ''
+      }]
+    })
+
+    var $paneHtml = dom.$editor.querySelector('.jotted-pane-html')
+    var containerWidth = parseInt(window.getComputedStyle(dom.$editor).getPropertyValue('width'), 10)
+    var paneWidth = parseInt(window.getComputedStyle($paneHtml).getPropertyValue('width'), 10)
+    expect(paneWidth).toBe(parseInt((containerWidth - 2) / 3, 10))
+  })
+
+  it('should take up 25% of the container with three file types', function () {
+    jotted.core = new Jotted(dom.$editor, {
+      files: [{
+        type: 'html',
+        content: ''
+      }, {
+        type: 'css',
+        content: ''
+      }, {
+        type: 'js',
+        content: ''
+      }]
+    })
+
+    var $paneHtml = dom.$editor.querySelector('.jotted-pane-html')
+    var containerWidth = parseInt(window.getComputedStyle(dom.$editor).getPropertyValue('width'), 10)
+    var paneWidth = parseInt(window.getComputedStyle($paneHtml).getPropertyValue('width'), 10)
+    expect(paneWidth).toBe(parseInt((containerWidth - 2) / 4, 10))
+  })
 })

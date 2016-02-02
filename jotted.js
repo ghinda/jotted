bf89e9fc7f75ba60e323b8ee0a0372afbaf949fc
Ionut Colceriu <contact@ghinda.net>
2016-02-02 10:39:33 +0200

    Built jotted from commit f91658f on branch tweaks

--- jotted.js
+++ jotted.js
@@ -281,8 +281,8 @@
     frameWindow.document.dispatchEvent(DOMContentLoadedEvent);
   }
 
-  // https://developer.mozilla.org/en/docs/Web/HTML/Element/script
-  var runScriptTypes = ['text/javascript', 'text/ecmascript', 'application/javascript', 'application/ecmascript'];
+  // https://html.spec.whatwg.org/multipage/scripting.html#javascript-mime-type
+  var runScriptTypes = ['application/javascript', 'application/ecmascript', 'application/x-ecmascript', 'application/x-javascript', 'text/ecmascript', 'text/javascript', 'text/javascript1.0', 'text/javascript1.1', 'text/javascript1.2', 'text/javascript1.3', 'text/javascript1.4', 'text/javascript1.5', 'text/jscript', 'text/livescript', 'text/x-ecmascript', 'text/x-javascript'];
 
   function runScripts($resultFrame) {
     var _this = this;

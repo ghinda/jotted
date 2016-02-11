1d0a157399b73a3c49ce2dfd1d6085ef5b1de996
Ionut Colceriu <contact@ghinda.net>
2016-02-11 15:55:52 +0200

    Built jotted from commit 0c463f5 on branch master

--- jotted.js
+++ jotted.js
@@ -241,6 +241,41 @@
     return node.getAttribute('data-' + attr);
   }
 
+  // mode detection based on content type and file extension
+  var defaultModemap = {
+    'html': 'html',
+    'css': 'css',
+    'js': 'javascript',
+    'less': 'less',
+    'styl': 'stylus',
+    'coffee': 'coffeescript'
+  };
+
+  function getMode() {
+    var type = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+    var file = arguments.length <= 1 || arguments[1] === undefined ? '' : arguments[1];
+    var customModemap = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+    var modemap = extend(customModemap, defaultModemap);
+
+    // try the file extension
+    for (var key in modemap) {
+      var keyLength = key.length;
+      if (file.slice(- keyLength++) === '.' + key) {
+        return modemap[key];
+      }
+    }
+
+    // try the file type (html/css/js)
+    for (var key in modemap) {
+      if (type === key) {
+        return modemap[key];
+      }
+    }
+
+    return type;
+  }
+
   var plugins = [];
 
   function find(id) {
@@ -383,6 +418,212 @@
       }
     }]);
     return PubSoup;
+  }();
+
+  var PluginConsole = function () {
+    function PluginConsole(jotted, options) {
+      babelHelpers.classCallCheck(this, PluginConsole);
+
+      options = extend(options, {});
+
+      var priority = 30;
+      var history = [];
+      var historyIndex = 0;
+
+      var $iframe = jotted.$container.querySelector('.jotted-pane-result iframe');
+
+      // new tab and pane markup
+      var $nav = document.createElement('li');
+      addClass($nav, 'jotted-nav-item jotted-nav-item-console');
+      $nav.innerHTML = '<a href="#" data-jotted-type="console">JS Console</a>';
+
+      var $pane = document.createElement('div');
+      addClass($pane, 'jotted-pane jotted-pane-console');
+
+      $pane.innerHTML = '\n      <div class="jotted-console-container">\n        <ul class="jotted-console-output"></ul>\n        <form class="jotted-console-input">\n          <input type="text">\n        </form>\n      </div>\n      <button class="jotted-console-clear">Clear</button>\n    ';
+
+      jotted.$container.appendChild($pane);
+      jotted.$container.querySelector('.jotted-nav').appendChild($nav);
+
+      var $container = jotted.$container.querySelector('.jotted-console-container');
+      var $output = jotted.$container.querySelector('.jotted-console-output');
+      var $input = jotted.$container.querySelector('.jotted-console-input input');
+      var $inputForm = jotted.$container.querySelector('.jotted-console-input');
+      var $clear = jotted.$container.querySelector('.jotted-console-clear');
+
+      // submit the input form
+      $inputForm.addEventListener('submit', this.submit.bind(this));
+
+      // console history
+      $input.addEventListener('keydown', this.history.bind(this));
+
+      // clear button
+      $clear.addEventListener('click', this.clear.bind(this));
+
+      // capture the console on each change
+      jotted.on('change', this.change.bind(this), priority);
+
+      // get log events from the iframe
+      window.addEventListener('message', this.getMessage.bind(this));
+
+      // plugin public properties
+      this.$container = $container;
+      this.$input = $input;
+      this.$output = $output;
+      this.$iframe = $iframe;
+      this.history = history;
+      this.historyIndex = historyIndex;
+    }
+
+    babelHelpers.createClass(PluginConsole, [{
+      key: 'getMessage',
+      value: function getMessage(e) {
+        // only catch messages from the iframe
+        if (e.source !== this.$iframe.contentWindow) {
+          return;
+        }
+
+        var data = JSON.parse(e.data);
+        if (data.type === 'jotted-console-log') {
+          this.log(data.message);
+        }
+      }
+    }, {
+      key: 'change',
+      value: function change(params, callback) {
+        // only parse js content
+        if (params.type !== 'js') {
+          // make sure we callback either way,
+          // to not break the pubsoup
+          return callback(null, params);
+        }
+
+        params.content = '(' + this.capture.toString() + ')();\n' + params.content;
+
+        callback(null, params);
+      }
+
+      // capture the console.log output
+
+    }, {
+      key: 'capture',
+      value: function capture() {
+        // IE9 with devtools closed
+        if (typeof window.console === 'undefined' || typeof window.console.log === 'undefined') {
+          window.console = {
+            log: function log() {}
+          };
+        }
+
+        // for IE9 support
+        var oldConsoleLog = Function.prototype.bind.call(window.console.log, window.console);
+
+        window.console.log = function () {
+          // send log messages to the parent window
+          [].slice.call(arguments).forEach(function (message) {
+            window.parent.postMessage(JSON.stringify({
+              type: 'jotted-console-log',
+              message: message
+            }), '*');
+          });
+
+          // in IE9, console.log is object instead of function
+          // https://connect.microsoft.com/IE/feedback/details/585896/console-log-typeof-is-object-instead-of-function
+          oldConsoleLog.apply(oldConsoleLog, arguments);
+        };
+      }
+    }, {
+      key: 'log',
+      value: function log() {
+        var message = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+        var type = arguments[1];
+
+        var $log = document.createElement('li');
+        addClass($log, 'jotted-console-log');
+
+        if (typeof type !== 'undefined') {
+          addClass($log, 'jotted-console-log-' + type);
+        }
+
+        $log.innerHTML = message;
+
+        this.$output.appendChild($log);
+      }
+    }, {
+      key: 'submit',
+      value: function submit(e) {
+        var inputValue = this.$input.value.trim();
+
+        // if input is blank, do nothing
+        if (inputValue === '') {
+          return e.preventDefault();
+        }
+
+        // add run to history
+        this.history.push(inputValue);
+        this.historyIndex = this.history.length;
+
+        // log input value
+        this.log(inputValue, 'history');
+
+        // add return if it doesn't start with it
+        if (inputValue.indexOf('return') !== 0) {
+          inputValue = 'return ' + inputValue;
+        }
+
+        // show output or errors
+        try {
+          // run the console input in the iframe context
+          var scriptOutput = this.$iframe.contentWindow.eval('(function() {' + inputValue + '})()');
+
+          this.log(scriptOutput);
+        } catch (err) {
+          this.log(err, 'error');
+        }
+
+        // clear the console value
+        this.$input.value = '';
+
+        // scroll console pane to bottom
+        // to keep the input into view
+        this.$container.scrollTop = this.$container.scrollHeight;
+
+        e.preventDefault();
+      }
+    }, {
+      key: 'clear',
+      value: function clear() {
+        this.$output.innerHTML = '';
+      }
+    }, {
+      key: 'history',
+      value: function history(e) {
+        var UP = 38;
+        var DOWN = 40;
+        var gotHistory = false;
+        var selectionStart = this.$input.selectionStart;
+
+        // only if we have previous history
+        // and the cursor is at the start
+        if (e.keyCode === UP && this.historyIndex !== 0 && selectionStart === 0) {
+          this.historyIndex--;
+          gotHistory = true;
+        }
+
+        // only if we have future history
+        // and the cursor is at the end
+        if (e.keyCode === DOWN && this.historyIndex !== this.history.length - 1 && selectionStart === this.$input.value.length) {
+          this.historyIndex++;
+          gotHistory = true;
+        }
+
+        // only if history changed
+        if (gotHistory) {
+          this.$input.value = this.history[this.historyIndex];
+        }
+      }
+    }]);
+    return PluginConsole;
   }();
 
   var PluginMarkdown = function () {
@@ -639,14 +880,6 @@
       this.editor = {};
       this.jotted = jotted;
 
-      this.modemap = {
-        'html': 'html',
-        'css': 'css',
-        'js': 'javascript',
-        'less': 'less',
-        'coffee': 'coffeescript'
-      };
-
       options = extend(options, {});
 
       // check if Ace is loaded
@@ -669,7 +902,7 @@
 
         var editorOptions = extend(options);
 
-        editor.getSession().setMode(this.aceMode(type, file));
+        editor.getSession().setMode('ace/mode/' + getMode(type, file));
         editor.getSession().setOptions(editorOptions);
 
         editor.$blockScrolling = Infinity;
@@ -707,27 +940,6 @@
         params.content = editor.getValue();
         callback(null, params);
       }
-    }, {
-      key: 'aceMode',
-      value: function aceMode(type, file) {
-        var mode = 'ace/mode/';
-
-        // try the file extension
-        for (var key in this.modemap) {
-          if (file.indexOf('.' + key) !== -1) {
-            return mode + this.modemap[key];
-          }
-        }
-
-        // try the file type (html/css/js)
-        for (var key in this.modemap) {
-          if (type === key) {
-            return mode + this.modemap[key];
-          }
-        }
-
-        return mode + type;
-      }
     }]);
     return PluginAce;
   }();
@@ -741,6 +953,11 @@
 
       this.editor = {};
       this.jotted = jotted;
+
+      // custom modemap for codemirror
+      var modemap = {
+        'html': 'htmlmixed'
+      };
 
       options = extend(options, {
         lineNumbers: true
@@ -756,8 +973,10 @@
       for (i = 0; i < $editors.length; i++) {
         var $textarea = $editors[i].querySelector('textarea');
         var type = data($textarea, 'jotted-type');
+        var file = data($textarea, 'jotted-file');
 
         this.editor[type] = window.CodeMirror.fromTextArea($textarea, options);
+        this.editor[type].setOption('mode', getMode(type, file, modemap));
       }
 
       jotted.on('change', this.change.bind(this), priority);
@@ -805,6 +1024,7 @@
     jotted.plugin('stylus', PluginStylus);
     jotted.plugin('babel', PluginBabel);
     jotted.plugin('markdown', PluginMarkdown);
+    jotted.plugin('console', PluginConsole);
   }
 
   var Jotted = function () {
@@ -885,7 +1105,6 @@
       }
 
       // change events
-      $container.addEventListener('change', debounce(this.change.bind(this), options.debounce));
       $container.addEventListener('keyup', debounce(this.change.bind(this), options.debounce));
 
       // pane change

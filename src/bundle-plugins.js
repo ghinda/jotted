1fb3dc8460ac3fbdeb7e42744a0ac790918fe85e
Arthur Moura Carvalho <armoucar@gmail.com>
2016-10-31 17:14:21 -0200

    colexpand plugin for flex containers

--- src/bundle-plugins.js
+++ src/bundle-plugins.js
@@ -14,6 +14,7 @@ import PluginBabel from './plugins/babel.js'
 import PluginMarkdown from './plugins/markdown.js'
 import PluginConsole from './plugins/console.js'
 import PluginPlay from './plugins/play.js'
+import PluginColExpand from './plugins/colexpand.js'
 
 export default function BundlePlugins (jotted) {
   jotted.plugin('render', PluginRender)
@@ -28,4 +29,5 @@ export default function BundlePlugins (jotted) {
   jotted.plugin('markdown', PluginMarkdown)
   jotted.plugin('console', PluginConsole)
   jotted.plugin('play', PluginPlay)
+  jotted.plugin('colexpand', PluginColExpand)
 }
---
+++ src/plugins/colexpand.js
@@ -0,0 +1,105 @@
+/* eslint-env browser */
+
+/* colexpand plugin
+ * adds a column expand functionality to the panes
+ */
+import * as util from '../util.js'
+
+export default class PluginColExpand {
+  constructor (jotted, options) {
+    this.jotted = jotted
+
+    options = util.extend(options, {})
+
+    this._querySelector = jotted.$container.querySelector.bind(jotted.$container)
+    this._querySelectorAll = jotted.$container.querySelectorAll.bind(jotted.$container)
+
+    // insert expander elements
+    Array.prototype.slice.call(this._querySelectorAll('.jotted-editor')).forEach(function (editor) {
+      let $colexpandElement = document.createElement('div')
+      $colexpandElement.classList.add('jotted-col-expand')
+
+      let $paneNavItem = editor.parentElement.querySelector('.jotted-nav-item')
+      editor.parentElement.insertBefore($colexpandElement, $paneNavItem)
+    })
+
+    // first: result pane
+    this.panes = [{
+      nav: this._querySelector('.jotted-nav-item-result'),
+      pane: this._querySelector('.jotted-pane-result'),
+      expander: undefined
+    }]
+
+    // code panes
+    for (let type of [ 'html', 'css', 'js' ]) {
+      let $paneType = this._querySelector(`.jotted-pane-${type}`)
+      let $pane = {
+        nav: this._querySelector(`.jotted-nav-item-${type}`),
+        container: this._querySelector(`.jotted-pane-${type}`),
+        expander: $paneType.querySelector('.jotted-col-expand')
+      }
+
+      this.panes.push($pane)
+
+      $pane.expander.addEventListener('mousedown', this.startExpand.bind(this, jotted))
+    }
+
+    jotted.$container.style.display = ''
+  }
+
+  startExpand (jotted, event) {
+    let $pane = this.panes
+      .filter((pane) => { return pane.expander === event.target })
+      .shift()
+
+    let $previousPane = this.panes[this.panes.indexOf($pane) - 1]
+
+    let $fixedSizePane = this.panes
+      .filter((pane) => { return pane !== $pane && pane !== $previousPane })
+      .pop()
+
+    $pane.startX = event.clientX
+    $pane.startWidth = parseFloat(getComputedStyle($pane.container).getPropertyValue('width'), 10)
+
+    $previousPane.startX = event.clientX
+    $previousPane.startWidth = parseFloat(getComputedStyle($previousPane.container).getPropertyValue('width'), 10)
+
+    $fixedSizePane.startWidth = parseFloat(getComputedStyle($fixedSizePane.container).getPropertyValue('width'), 10)
+
+    $pane.mousemove = this.doDrag.bind(this, $pane, $previousPane, $fixedSizePane)
+    $pane.mouseup = this.stopDrag.bind(this, $pane)
+
+    document.addEventListener('mousemove', $pane.mousemove, false)
+    document.addEventListener('mouseup', $pane.mouseup, false)
+  }
+
+  doDrag (pane, previousPane, fixedSizePane, event) {
+    // previous pane
+    let ppNewWidth = (previousPane.startWidth + event.clientX - pane.startX)
+
+    // current pane
+    let cpNewWidth = (pane.startWidth - event.clientX + pane.startX)
+
+    // contracting a pane are restricted to a min-size of 100px.
+    const PANE_MIN_SIZE = 100
+    if (ppNewWidth >= PANE_MIN_SIZE && cpNewWidth >= PANE_MIN_SIZE) {
+      [pane, previousPane, fixedSizePane]
+        .forEach((p) => { this.overrideFixedSizeAttributes(p) })
+
+      previousPane.container.style.width = `${ppNewWidth}px`
+      pane.container.style.width = `${cpNewWidth}px`
+      fixedSizePane.container.style.width = `${fixedSizePane.startWidth}px`
+    }
+  }
+
+  stopDrag (pane, event) {
+    document.removeEventListener('mousemove', pane.mousemove, false)
+    document.removeEventListener('mouseup', pane.mouseup, false)
+  }
+
+  overrideFixedSizeAttributes (pane) {
+    pane.container.style.flex = '0 0 auto'
+    pane.container.style.maxWidth = 'none'
+  }
+}
+
